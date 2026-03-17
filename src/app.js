dc9f5f0d00b5e5c10918d9c40e1a37a127d021e2
aymnr <aymnryadmhmwdmhmd@gmail.com>
2026-03-17 23:06:19 +0300

    refresh token

--- src/app.js
+++ src/app.js
@@ -77,10 +77,9 @@ app.use("/api/series",     seriesRoutes);
 // ─── Static Files
 app.use("/uploads", express.static("uploads"));
 
-app.get("/api/auth/google/callback", (req, res) => {
+/*app.get("/api/auth/google/callback", (req, res) => {
     res.json({ code: req.query.code });
-});
-
+});*/ 
 app.use(notFound);
 app.use(errorHandler);
 
