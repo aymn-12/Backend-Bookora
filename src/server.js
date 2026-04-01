a93e9a46a2c544b981ab94993a54f0a55c99432f
Mr.Q <aymnryadmhmwdmhmd@gmail.com>
2026-04-01 09:27:54 +0300

    Refactor server startup to await DB connection

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const app = require('./app');
-const { connectDB } = require('./config/db')
-const cleanExpiredOTPS = require("./utils/cleanExpiredOTPs.utils")
-const databaseHealthCheckJob = require("./utils/healthCheck.job")
+const { connectDB } = require('./config/db');
+const cleanExpiredOTPS = require("./utils/cleanExpiredOTPs.utils");
+const databaseHealthCheckJob = require("./utils/healthCheck.job");
+
 const PORT = process.env.PORT || 3000;
 
-cleanExpiredOTPS()
-databaseHealthCheckJob()
-connectDB()
+const startServer = async () => {
+  await connectDB();           // ← ينتظر DB أولاً
 
-app.listen(PORT, () => {
+  cleanExpiredOTPS();          // ← بعد DB جاهزة
+  databaseHealthCheckJob();    // ← بعد DB جاهزة
+
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+  });
+};
+
+startServer();
